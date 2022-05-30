fefd241375271d495e5ed8aaa7d91286ee750848
MauricioInostrozaA <mauricio.inostroza1@mail.udp.cl>
2022-05-30 12:04:07 -0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const memjs = require('memjs');
 const axios = require("axios");
-const client = memjs.Client.create('memcached:11211');
+const HashRing = require('node-hashring');
+
+//Setting different servers
+const servers = ['memcached:11211','memcached2:11211','memcached3:11211'];
+const serverMap = {}
+servers.forEach((server, index) => serverMap[server] = index);
+
+const client = memjs.Client.create(servers.join(','));
+//const client = memjs.Client.create('memcached:11211');
+
+//Setting the hash ring
+const hashRing = new HashRing(servers);
+
+client.getServer = (key) => serverMap[hashRing.findNode(key)];
+
 const responseTime = require("response-time");
 
 const port = 3000;
@@ -21,7 +35,7 @@ app.get("/character/:id", async(req,res,next) =>{
             console.log(response.data);
             await client.set(req.params.id, response.data.name, {expires:600}); 
             console.log("db");
-            res.send(response.data.name);      
+            res.send(response.data.name); 
         }
         else{
             console.log("cache");
